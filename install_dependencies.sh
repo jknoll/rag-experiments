29e9005a5d8ec9ac90eb23cf47aa69723d254cbf
Justin F. Knoll <justin.knoll@gmail.com>
2024-03-20 13:13:27 -0700

    added main RAG script

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-python3 -m pip install langchain openai weaviate-client
+python3 -m pip install langchain openai weaviate-client dotenv
 
---
+++ main.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+import dotenv
+dotenv.load_dotenv()
+
+import requests
+from langchain.document_loaders import TextLoader
+
+url = "https://raw.githubusercontent.com/langchain-ai/langchain/master/docs/docs/modules/state_of_the_union.txt"
+res = requests.get(url)
+with open("state_of_the_union.txt", "w") as f:
+    f.write(res.text)
+
+loader = TextLoader('./state_of_the_union.txt')
+documents = loader.load()
+
+from langchain.text_splitter import CharacterTextSplitter
+text_splitter = CharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+chunks = text_splitter.split_documents(documents)
+
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import Weaviate
+import weaviate
+from weaviate.embedded import EmbeddedOptions
+
+client = weaviate.Client(
+    embedded_options = EmbeddedOptions
+)
+
+vectorstore = Weaviate.from_documents(
+    client = client,
+    documents = chunks,
+    embedding = OpenAIEmbeddings(),
+    by_text = False
+)
+
+retriever = vectorstore.as_retriever()
+
+from langchain.prompts import ChatPromptTemplate
+
+template = """You are an assistant for question-answering tasks. 
+Use the following pieces of retrieved context to answer the question. 
+If you don't know the answer, just say that you don't know. 
+Use three sentences maximum and keep the answer concise.
+Question: {question} 
+Context: {context} 
+Answer:
+"""
+
+prompt = ChatPromptTemplate.from_template(template)
+
+print(prompt)
